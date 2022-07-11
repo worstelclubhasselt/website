0a71c9cf7db0910364f582c50aeab7b928545943
HursitTarcan <hursittarcan@gmail.com>
2022-07-11 21:37:41 +0200

    javascript changed to source script from template

--- docs/app.js
+++ docs/app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 250) {
-        header.style.backgroundColor = 'black';
+        header.style.backgroundColor = '#29323c';
     } else {
         header.style.backgroundColor = 'transparent';
     }
